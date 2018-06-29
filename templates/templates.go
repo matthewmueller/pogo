4f1c5d8ab47f78026e54e34136c4d75461bc9c5c
Matthew Mueller <mattmuelle@gmail.com>
2018-06-30 00:05:24 +0700

    update templates with new go-bindata

--- templates/templates.go
+++ templates/templates.go
@@ -4,11 +4,13 @@
 // templates/many.gotmpl
 // templates/model.gotmpl
 // templates/pogo.gotmpl
+
 package templates
 
 import (
 	"bytes"
 	"compress/gzip"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -39,8 +41,9 @@ func bindataRead(data []byte, name string) ([]byte, error) {
 }
 
 type asset struct {
-	bytes []byte
-	info  os.FileInfo
+	bytes  []byte
+	info   os.FileInfo
+	digest [sha256.Size]byte
 }
 
 type bindataFileInfo struct {
@@ -85,7 +88,7 @@ func templatesEnumGotmpl() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "templates/enum.gotmpl", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
-	a := &asset{bytes: bytes, info: info}
+	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x1, 0x80, 0x5c, 0xe4, 0x7a, 0x54, 0x28, 0x97, 0x98, 0x42, 0xcd, 0x72, 0xfd, 0x46, 0x3c, 0xf2, 0xe6, 0x53, 0xd4, 0x46, 0x73, 0x27, 0xa8, 0xec, 0xb0, 0xf, 0x14, 0x9c, 0xdb, 0xa, 0xca, 0xa2}}
 	return a, nil
 }
 
@@ -105,7 +108,7 @@ func templatesManyGotmpl() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "templates/many.gotmpl", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
-	a := &asset{bytes: bytes, info: info}
+	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x63, 0xbe, 0x29, 0x19, 0xc3, 0x4e, 0x35, 0x46, 0x33, 0x66, 0x7c, 0x92, 0xc7, 0x5b, 0xb0, 0x18, 0x13, 0x5, 0x9b, 0xe1, 0xb6, 0x76, 0x2e, 0x1f, 0xdf, 0xd3, 0xc8, 0x86, 0x59, 0xa4, 0x35, 0x95}}
 	return a, nil
 }
 
@@ -125,7 +128,7 @@ func templatesModelGotmpl() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "templates/model.gotmpl", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
-	a := &asset{bytes: bytes, info: info}
+	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x5e, 0x7c, 0xa9, 0x91, 0x9c, 0xf5, 0x53, 0xa5, 0x82, 0xdb, 0xef, 0x4, 0x51, 0x33, 0x95, 0x51, 0x79, 0x4a, 0xeb, 0x1c, 0x59, 0x9d, 0xd6, 0x79, 0xea, 0x84, 0x7f, 0xdf, 0xe1, 0x25, 0xe3, 0x2b}}
 	return a, nil
 }
 
@@ -145,7 +148,7 @@ func templatesPogoGotmpl() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "templates/pogo.gotmpl", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
-	a := &asset{bytes: bytes, info: info}
+	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x5d, 0xeb, 0xc6, 0xd9, 0xb1, 0xeb, 0x52, 0x4d, 0x2f, 0x29, 0x7e, 0x2b, 0x49, 0x32, 0x8e, 0x1a, 0x84, 0x29, 0x28, 0x34, 0x5c, 0xd7, 0xd6, 0x27, 0x4b, 0x11, 0x1d, 0x5e, 0xa4, 0xee, 0xc6, 0x47}}
 	return a, nil
 }
 
@@ -153,8 +156,8 @@ func templatesPogoGotmpl() (*asset, error) {
 // It returns an error if the asset could not be found or
 // could not be loaded.
 func Asset(name string) ([]byte, error) {
-	cannonicalName := strings.Replace(name, "\\", "/", -1)
-	if f, ok := _bindata[cannonicalName]; ok {
+	canonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[canonicalName]; ok {
 		a, err := f()
 		if err != nil {
 			return nil, fmt.Errorf("Asset %s can't read by error: %v", name, err)
@@ -162,6 +165,12 @@ func Asset(name string) ([]byte, error) {
 		return a.bytes, nil
 	}
 	return nil, fmt.Errorf("Asset %s not found", name)
+}
+
+// AssetString returns the asset contents as a string (instead of a []byte).
+func AssetString(name string) (string, error) {
+	data, err := Asset(name)
+	return string(data), err
 }
 
 // MustAsset is like Asset but panics when Asset would return an error.
@@ -175,12 +184,18 @@ func MustAsset(name string) []byte {
 	return a
 }
 
+// MustAssetString is like AssetString but panics when Asset would return an
+// error. It simplifies safe initialization of global variables.
+func MustAssetString(name string) string {
+	return string(MustAsset(name))
+}
+
 // AssetInfo loads and returns the asset info for the given name.
 // It returns an error if the asset could not be found or
 // could not be loaded.
 func AssetInfo(name string) (os.FileInfo, error) {
-	cannonicalName := strings.Replace(name, "\\", "/", -1)
-	if f, ok := _bindata[cannonicalName]; ok {
+	canonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[canonicalName]; ok {
 		a, err := f()
 		if err != nil {
 			return nil, fmt.Errorf("AssetInfo %s can't read by error: %v", name, err)
@@ -188,6 +203,33 @@ func AssetInfo(name string) (os.FileInfo, error) {
 		return a.info, nil
 	}
 	return nil, fmt.Errorf("AssetInfo %s not found", name)
+}
+
+// AssetDigest returns the digest of the file with the given name. It returns an
+// error if the asset could not be found or the digest could not be loaded.
+func AssetDigest(name string) ([sha256.Size]byte, error) {
+	canonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[canonicalName]; ok {
+		a, err := f()
+		if err != nil {
+			return [sha256.Size]byte{}, fmt.Errorf("AssetDigest %s can't read by error: %v", name, err)
+		}
+		return a.digest, nil
+	}
+	return [sha256.Size]byte{}, fmt.Errorf("AssetDigest %s not found", name)
+}
+
+// Digests returns a map of all known files and their checksums.
+func Digests() (map[string][sha256.Size]byte, error) {
+	mp := make(map[string][sha256.Size]byte, len(_bindata))
+	for name := range _bindata {
+		a, err := _bindata[name]()
+		if err != nil {
+			return nil, err
+		}
+		mp[name] = a.digest
+	}
+	return mp, nil
 }
 
 // AssetNames returns the names of the assets.
@@ -202,8 +244,11 @@ func AssetNames() []string {
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
 	"templates/enum.gotmpl": templatesEnumGotmpl,
+
 	"templates/many.gotmpl": templatesManyGotmpl,
+
 	"templates/model.gotmpl": templatesModelGotmpl,
+
 	"templates/pogo.gotmpl": templatesPogoGotmpl,
 }
 
@@ -216,15 +261,15 @@ var _bindata = map[string]func() (*asset, error){
 //       img/
 //         a.png
 //         b.png
-// then AssetDir("data") would return []string{"foo.txt", "img"}
-// AssetDir("data/img") would return []string{"a.png", "b.png"}
-// AssetDir("foo.txt") and AssetDir("notexist") would return an error
+// then AssetDir("data") would return []string{"foo.txt", "img"},
+// AssetDir("data/img") would return []string{"a.png", "b.png"},
+// AssetDir("foo.txt") and AssetDir("notexist") would return an error, and
 // AssetDir("") will return []string{"data"}.
 func AssetDir(name string) ([]string, error) {
 	node := _bintree
 	if len(name) != 0 {
-		cannonicalName := strings.Replace(name, "\\", "/", -1)
-		pathList := strings.Split(cannonicalName, "/")
+		canonicalName := strings.Replace(name, "\\", "/", -1)
+		pathList := strings.Split(canonicalName, "/")
 		for _, p := range pathList {
 			node = node.Children[p]
 			if node == nil {
@@ -246,16 +291,17 @@ type bintree struct {
 	Func     func() (*asset, error)
 	Children map[string]*bintree
 }
+
 var _bintree = &bintree{nil, map[string]*bintree{
 	"templates": &bintree{nil, map[string]*bintree{
-		"enum.gotmpl": &bintree{templatesEnumGotmpl, map[string]*bintree{}},
-		"many.gotmpl": &bintree{templatesManyGotmpl, map[string]*bintree{}},
+		"enum.gotmpl":  &bintree{templatesEnumGotmpl, map[string]*bintree{}},
+		"many.gotmpl":  &bintree{templatesManyGotmpl, map[string]*bintree{}},
 		"model.gotmpl": &bintree{templatesModelGotmpl, map[string]*bintree{}},
-		"pogo.gotmpl": &bintree{templatesPogoGotmpl, map[string]*bintree{}},
+		"pogo.gotmpl":  &bintree{templatesPogoGotmpl, map[string]*bintree{}},
 	}},
 }}
 
-// RestoreAsset restores an asset under the given directory
+// RestoreAsset restores an asset under the given directory.
 func RestoreAsset(dir, name string) error {
 	data, err := Asset(name)
 	if err != nil {
@@ -273,14 +319,10 @@ func RestoreAsset(dir, name string) error {
 	if err != nil {
 		return err
 	}
-	err = os.Chtimes(_filePath(dir, name), info.ModTime(), info.ModTime())
-	if err != nil {
-		return err
-	}
-	return nil
+	return os.Chtimes(_filePath(dir, name), info.ModTime(), info.ModTime())
 }
 
-// RestoreAssets restores an asset under the given directory recursively
+// RestoreAssets restores an asset under the given directory recursively.
 func RestoreAssets(dir, name string) error {
 	children, err := AssetDir(name)
 	// File
@@ -298,7 +340,6 @@ func RestoreAssets(dir, name string) error {
 }
 
 func _filePath(dir, name string) string {
-	cannonicalName := strings.Replace(name, "\\", "/", -1)
-	return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
+	canonicalName := strings.Replace(name, "\\", "/", -1)
+	return filepath.Join(append([]string{dir}, strings.Split(canonicalName, "/")...)...)
 }
-
